ed027588bb37521eb074d74a16e043592d31e6c6
cellcheapmeals-hub <cellcheapmeals@gmail.com>
2025-10-27 21:06:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // === CONFIG ===
-const sheetURL = "https://docs.google.com/spreadsheets/d/e/PASTE-YOUR-LINK-HERE/pub?output=csv";
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPXZ6M20Zh0YZkq60NtJSYZ2rv3J-hravmeyeiaTOwtprq1EjrU4St0rQCXvYiUCNp5Sy47AMAoxEW/pub?gid=0&single=true&output=csv";
 
 // Lab coordinates
 const lab = { name: "TU Wien Biomedical Lab", lat: 48.1987, lng: 16.3695 };
